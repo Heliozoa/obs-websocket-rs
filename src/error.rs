510f79233064a3e305698ecec946f040e46e4c45
Heliozoa <dm89132@gmail.com>
2020-02-04 18:52:28 +0200

    improved error handling

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,14 @@
 use serde_json::error::Error as JsonError;
 use tungstenite::error::Error as TungsteniteError;
+use tungstenite::handshake::{HandshakeError, HandshakeRole};
 use url::ParseError;
 
 #[derive(Debug)]
 pub enum Error {
     Custom(String),
     ObsError(String),
+    HandshakeInterrupted,
+    HandshakeFailed(TungsteniteError),
     Tungstenite(TungsteniteError),
     Json(JsonError),
     Parse(ParseError),
@@ -26,5 +29,14 @@ impl From<JsonError> for Error {
 impl From<ParseError> for Error {
     fn from(err: ParseError) -> Error {
         Error::Parse(err)
+    }
+}
+
+impl<T: HandshakeRole> From<HandshakeError<T>> for Error {
+    fn from(err: HandshakeError<T>) -> Error {
+        match err {
+            HandshakeError::Failure(err) => Error::HandshakeFailed(err),
+            HandshakeError::Interrupted(_) => Error::HandshakeInterrupted,
+        }
     }
 }
